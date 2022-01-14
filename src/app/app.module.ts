e6cec57ef21e2bbd2360ca213f21c2a4a9ba23a2
Francisco Zambrano <fzambran@bancolombia.com.co>
2022-01-14 16:04:13 -0500

    feat: template initialized
    Francisco Zambrano

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { NewsComponent } from './components/news/news.component';
+import { SuggestedComponent } from './components/suggested/suggested.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    NewsComponent,
+    SuggestedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/suggested/suggested.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'suggested',
+  templateUrl: './suggested.component.html',
+  styleUrls: ['./suggested.component.scss']
+})
+export class SuggestedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
